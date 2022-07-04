ff86302d0c45b532b6169db4297b21225b6145f3
Gonçalo Ferreira Silva <goncalo.silva.22679@formandos.cinel.pt>
2022-07-04 17:01:18 +0100

    First controller

---
+++ SuperShopGS/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShopGS.Data.Entities;
+
+namespace SuperShopGS.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
