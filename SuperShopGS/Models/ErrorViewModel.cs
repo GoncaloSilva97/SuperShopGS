8fed17c3b648aaa462233c5fe844c6f63c0f5d5f
Gonçalo Ferreira Silva <goncalo.silva.22679@formandos.cinel.pt>
2022-07-04 14:50:27 +0100

    Adicionar arquivos de projeto.

---
+++ SuperShopGS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SuperShopGS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
