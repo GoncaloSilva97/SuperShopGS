bdae501d86fe80e164a98dcb3dac080feb94ac5e
Gonçalo Ferreira Silva <goncalo.silva.22679@formandos.cinel.pt>
2022-07-27 16:41:59 +0100

    Modify products

--- SuperShopGS/Data/Entities/Product.cs
+++ SuperShopGS/Data/Entities/Product.cs
@@ -7,6 +7,10 @@ namespace SuperShopGS.Data.Entities
     {
         [Key]
         public int Id { get; set; }
+
+
+        [Required]
+        [MaxLength(50, ErrorMessage ="The filde {0} can contain {1} Characteres length.")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:c2}", ApplyFormatInEditMode = false)]
@@ -16,10 +20,10 @@ namespace SuperShopGS.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
