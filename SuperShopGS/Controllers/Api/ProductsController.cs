c1c9c972764e449794eeb8bcb062a1888a693ceb
Gonçalo Ferreira Silva <goncalo.silva.22679@formandos.cinel.pt>
2022-08-03 20:14:11 +0100

    Api Products Controller

---
+++ SuperShopGS/Controllers/Api/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperShopGS.Data;
+
+namespace SuperShopGS.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+
+        }
+
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok(_productRepository.GetAll());
+        }
+
+
+    }
+}
